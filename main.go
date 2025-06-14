936dba02b7f35f3798194ad79a10b8c5eb9af96b
alonyaska <130164070+alonyaska@users.noreply.github.com>
2025-06-14 23:22:28 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,77 +1,20 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
-	"io"
+	cryptoprice "golang-alonya/cryptoPrices/CryptoPrice"
 	"log"
-	"net/http"
-	"net/http/cookiejar"
-	"os"
-	"time"
+	// путь зависит от твоего go.mod
 )
 
-type loggingRoundTripper struct {
-	logger io.Writer
-	next   http.RoundTripper
-}
-
-type authRoundTripper struct {
-	token string
-	next  http.RoundTripper
-}
-
-func (l loggingRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	fmt.Fprintf(l.logger, "[%s] %s %s \n ", time.Now().Format(time.ANSIC), r.Method, r.URL)
-	return l.next.RoundTrip(r)
-}
-func (a authRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	r.Header.Set("Authorization", "Bearer "+a.token)
-	fmt.Println(string(a.token))
-	return a.next.RoundTrip(r)
-}
-func generateToken() string {
-	b := make([]byte, 8) // даст токен длиной 16 символов
-	rand.Read(b)
-	return hex.EncodeToString(b)
-}
-
 func main() {
-	jar, _ := cookiejar.New(nil) 
-
-	token := generateToken()
-
-	client := &http.Client{
-		Jar: jar,
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			fmt.Println(req.Response.Status)
-			fmt.Println("redirect")
-			return nil
-		},
-		Transport: &loggingRoundTripper{
-			logger: os.Stdout,
-			next: authRoundTripper{
-				token: token,
-				next:  http.DefaultTransport,
-
-			
-			},
-		},
-	}
-
-	resp, err := client.Get("http://httpbin.org/get")
+	assets, err := cryptoprice.FetchAssets()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer resp.Body.Close()
-
-	fmt.Println("response status -", resp.StatusCode)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	for _, asset := range assets {
+		fmt.Println(asset.Info())
 	}
-	fmt.Println(string(body))
 }
+
